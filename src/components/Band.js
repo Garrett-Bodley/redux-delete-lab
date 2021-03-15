60bb4c0582e3adf2af21a1d89a80fa5e5588c46d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-15 16:59:01 -0400

    Finished lab.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
-
+  
   render() {
     return(
-      <div>
-        Band Component
-      </div>
+    <li>
+      {this.props.band.name}  <button onClick={() => this.props.delete(this.props.band.id)}>delete</button>
+    </li>
     );
   }
 };
